e59b194d9174f33f1e752227d927235dbfb8a8da
DanaMedany <DanaMedany94@gmail.com>
2026-01-17 23:50:54 +0400

    fix: fixing type line issue in parallax carousel project component

--- src/components/custom/SubPagesHero.tsx
+++ src/components/custom/SubPagesHero.tsx
@@ -1,10 +1,15 @@
 import { cn } from "@/lib/utils";
+import Image from "next/image";
+
 const SubPagesHero = ({ children, bgImage }: { children: React.ReactNode; bgImage: string }) => {
   return (
-    <section
-      className={cn("relative w-full h-100 bg-cover bg-center")}
-      style={{ backgroundImage: `url('${bgImage}')` }}
-    >
+    <section className={cn("relative w-full h-[50vh]")}>
+      <Image
+        src={bgImage}
+        alt="Hero background"
+        fill
+        className="object-cover object-center -z-10"
+      />
       <div className="overlay"></div>
       <div className="container relative z-20 h-full flex flex-col items-center justify-center text-center">
         {children}
